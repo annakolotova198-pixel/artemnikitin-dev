1dfd02a139a2a33846a638fdb3b2d4bd87dfba64
annakolotova198-pixel <annakolotova198@gmail.com>
2026-07-30 15:24:44 +0300

    Start autonomous Dzen content worker

--- telegram_web.py
+++ telegram_web.py
@@ -3,6 +3,7 @@
 import os
 
 from app import app
+from dzen_content_service import install_routes, start_dzen_content_worker
 
 if os.getenv("TG_MODE", "user").strip().lower() == "bot":
     from telegram_bot_api import start_background_parser
@@ -11,4 +12,5 @@ else:
 
 
 start_background_parser()
-
+install_routes(app)
+start_dzen_content_worker()
